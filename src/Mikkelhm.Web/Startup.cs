7128225b2961bae8e4ddcf4d9eb8c60ef0a99ae6
mikkelhm <mikkel@h-madsen.dk>
2023-06-29 22:14:59 +0200

    Enable the ContentDeliveryApi

--- src/Mikkelhm.Web/Startup.cs
+++ src/Mikkelhm.Web/Startup.cs
@@ -32,6 +32,7 @@ namespace UmbracoProject
             services.AddUmbraco(_env, _config)
                 .AddBackOffice()
                 .AddWebsite()
+                .AddDeliveryApi()
                 .AddComposers()
                 .Build();
         }
